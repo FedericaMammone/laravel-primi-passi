82b5f3e1e8c94965a6268cab88b301af8d8613c2
Fef97 <federicakr97@gmail.com>
2023-07-14 18:40:24 +0200

    primo esercizio completato

---
+++ resources/views/home.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>Ciao!</h1>
+
+    <ul>
+        @foreach($students as $student)
+
+        <li>
+            {{$student}}
+        </li>
+        @endforeach
+    </ul>
+</body>
+
+</html>
